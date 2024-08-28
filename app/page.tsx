937ca37843a443be7ef39b8f2e412395f349401a
Rohan Mathur <rm30092000mr@gmail.com>
2024-08-28 23:32:51 +0530

    fix-bug: Toggle between Solana & Ethereum Wallets

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Navbar from "@/components/Navbar";
-import WalletGenerator from "@/components/WalletGenerator";
+import MainComponent from "@/components/MainComponent";
 
 export default function Home() {
   return (
     <main className="max-w-7xl mx-auto flex flex-col gap-4 p-4 min-h-[92vh]">
       <Navbar />
-      <WalletGenerator />
+      <MainComponent />
     </main>
   );
 }
